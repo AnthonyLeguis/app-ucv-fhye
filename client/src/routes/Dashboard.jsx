2bf13a8a5c3571c364e203eef62d14182ecf67fd
Anthony Leguisamo <anthony.leguisamo.g@outlook.com>
2024-07-22 11:57:30 -0400

    titulos del sidebar

--- client/src/routes/Dashboard.jsx
+++ client/src/routes/Dashboard.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import '../CSS/dasboard.css'
 
 export const Dashboard = () => {
   return (
     <>
-        <div className="container-fluid">
-            <h1>Estadísticas</h1>
+        <div className='container-fluid contain overflow-auto'>
+        <div className='row h-100 sm-flex-column'>
+          <div className="col mt-4 mb-2 rounded-1 col-md-8 mx-auto d-flex flex-row justify-content-center justify-content-md-right">
+            <h1 className='TitleName text-center text-center m-0'>Configuración de usuario</h1>
+            </div>
+          </div>
         </div>
     </>
   )
--- client/src/routes/SetupUser.jsx
+++ client/src/routes/SetupUser.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import '../CSS/setupUser.css'
 
 export const SetupUser = () => {
   return (
     <>
-        <div>
-            <h1>Configuracion de usuario</h1>
+      <div className='container-fluid contain overflow-auto'>
+        <div className='row h-100 sm-flex-column'>
+          <div className="col mt-4 mb-2 rounded-1 col-md-8 mx-auto d-flex flex-row justify-content-center justify-content-md-right">
+            <h1 className='TitleName text-center text-center m-0'>Configuración de usuario</h1>
+            </div>
+          </div>
         </div>
-    </>
-  )
+      </>
+      )
 }
